0f17e3ca23e0c7bcbe24ef5cf0fe5b726d4ba38f
Sayed Hasan <sayed.hasan@equionconsutling.com>
2020-02-01 16:31:34 +0000

    - Add Search input comp - Add Search results comp

--- src/containers/portfolio/Portfolio.test.tsx
+++ src/containers/portfolio/Portfolio.test.tsx
@@ -1,7 +1,7 @@
 import configureMockStore from "redux-mock-store";
 import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import ConnectedPortfolio from "./Portfolio";
+import ConnectedPortfolio, { Portfolio, IPortfolioProps } from "./Portfolio";
 import React from "react";
 import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
@@ -13,7 +13,7 @@ describe.only("Portfolio Component", () => {
     function setup() {
       const mockStore = configureMockStore();
       const store = mockStore({ portfolio: [1] });
-      const wrapper = render(
+      const wrapper = mount(
         <Provider store={store}>
           <ConnectedPortfolio />
         </Provider>
@@ -22,10 +22,70 @@ describe.only("Portfolio Component", () => {
       return wrapper;
     }
 
-    it("Should inject portfolio state in the component as expected", async () => {
-      const wrapper = setup();
-      // let prop =
-      // expect(prop).toBeTruthy()
+    // it("Should inject portfolio state in the component as expected", async () => {
+    //   const wrapper = setup();
+    //   // let prop =
+    //   // expect(prop).toBeTruthy()
+    // });
+
+    it("Should render without error", () => {
+      let wrapper = setup();
+      let portfolioComp = wrapper.find(`[data-test="portfolio-comp"]`);
+      expect(portfolioComp.length).toEqual(1);
+    });
+  });
+
+  describe("Portfolio with Props", () => {
+    function setup(props: IPortfolioProps) {
+      return shallow(<Portfolio {...props} />);
+    }
+
+    let wrapper;
+    let stocks: IStock[] = [
+      {
+        id: 1,
+        name: "Tesco"
+      },
+      {
+        id: 2,
+        name: "Next"
+      },
+
+      {
+        id: 3,
+        name: "Asos"
+      }
+    ];
+    let portfolio = [];
+    beforeAll(() => {
+      wrapper = setup({
+        portfolio,
+        stocks
+      });
+    });
+
+    it("Should be able to search for stock", done => {
+      let searchComp = wrapper.find("SearchInput").dive();
+
+      searchComp.find("input").simulate("change", { target: { value: "Tesco" } });
+
+      setTimeout(() => {
+        expect(wrapper.state()["searchText"]).toEqual("Tesco");
+        done();
+      }, 270);
+    });
+
+    it("Should be able to display search results", done => {
+      let searchComp = wrapper.find("SearchInput").dive();
+
+      searchComp.find("input").simulate("change", { target: { value: "Tesco" } });
+      setTimeout(() => {
+        let searchResultsComp = wrapper.find("SearchResults").dive();
+        expect(searchResultsComp.length).toEqual(1);
+        let result = searchResultsComp.find(".result").first();
+        expect(result.text()).toEqual("Tesco");
+        done();
+      }, 270);
     });
   });
 });
--- src/containers/portfolio/Portfolio.tsx
+++ src/containers/portfolio/Portfolio.tsx
@@ -1,36 +1,62 @@
 import * as React from "react";
-import { IPortfolioItem } from "../../reducers/portfolio";
 import { connect } from "react-redux";
 import SearchInput from "./../../components/search-input/SearchInput";
+import { SearchResults } from "../../components/search-results/SearchResults";
+import { Subject } from "rxjs";
 
 export interface IPortfolioProps {
   portfolio: IPortfolioItem[];
+  stocks: IStock[];
 }
 
 export class Portfolio extends React.Component<IPortfolioProps> {
+  public state = {
+    searchText: "",
+    searchResults: []
+  };
+
+  private clearSearchResultEvent = new Subject<void>();
   constructor(public props: IPortfolioProps) {
     super(props);
   }
 
-  private onChange = (text: string) => {
+  private search = (text: string) => {
     this.setState({
       searchText: text
     });
+    text = text.toLowerCase();
+    let searchResults = this.props.stocks.filter(x => text && x.name.toLocaleLowerCase().includes(text));
+
+    this.setState({
+      searchResults
+    });
+  };
+
+  private selectSearchResult = item => {
+    this.clearSearchResultEvent.next();
   };
   public render() {
+    let { searchResults } = this.state;
     return (
-      <div>
-        <SearchInput type="text" placeholder="Search..." value="" onChange={this.onChange} />
+      <div data-test="portfolio-comp">
+        <SearchInput
+          type="text"
+          placeholder="Search..."
+          value=""
+          onChange={this.search}
+          clear$={this.clearSearchResultEvent}
+        />
+        <SearchResults results={searchResults} select={this.selectSearchResult} displayKey="name" keyId="id" />
       </div>
     );
   }
 }
 
 const mapStateToProps = (state: any): IPortfolioProps => {
-  console.log(state);
-  let portfolio = state.portfolio as IPortfolioItem[];
+  let { portfolio, stocks } = state;
   return {
-    portfolio
+    portfolio,
+    stocks
   };
 };
 
